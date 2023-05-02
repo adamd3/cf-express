3c131e44b44547d28f3ee4e13b83d264e1c445a9
Adam Dinan <adam1989ie@gmail.com>
2023-05-02 18:24:42 +0100

    Set up frontend to consume API

--- cf-frontend/src/components/geneExpression.js
+++ cf-frontend/src/components/geneExpression.js
@@ -2,32 +2,78 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function GeneExpression() {
-  const [geneName, setGeneName] = useState('');
-  const [expressionValue, setExpressionValue] = useState(null);
+  const [gene, setGene] = useState('');
+  const [group, setGroup] = useState('');
+  const [timepoint, setTimepoint] = useState('');
+  const [cell, setCellType] = useState('');
+  const [expressionValues, setExpressionValues] = useState([]);
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const response = await axios.get('/api/gene_expression', {
-      params: { gene_name: geneName },
-    });
-    setExpressionValue(response.data.expression_value);
+    axios
+      // .get('/api/gene_expression', {
+      .get('http://127.0.0.1:5000/api/gene_expression', {
+        params: {
+          gene: gene,
+          cell: cell,
+        },
+      })
+      .then((response) => {
+        setExpressionValues(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <div>
+      <h1>Gene Expression</h1>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={geneName}
-          onChange={(e) => setGeneName(e.target.value)}
-        />
+        <div>
+          <label htmlFor="cellType">Cell Type:</label>
+          <input
+            type="text"
+            id="cell"
+            value={cell}
+            onChange={(event) => setCellType(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="gene">Gene:</label>
+          <input
+            type="text"
+            id="gene"
+            value={gene}
+            onChange={(event) => setGene(event.target.value)}
+          />
+        </div>
         <button type="submit">Submit</button>
       </form>
-      {expressionValue && (
-        <p>
-          The expression value of {geneName} is
-          {expressionValue}.
-        </p>
+
+      {expressionValues.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Gene ID</th>
+              <th>Group ID</th>
+              <th>Timepoint ID</th>
+              <th>Cell Type</th>
+              <th>Expression Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expressionValues.map((row, index) => (
+              <tr key={index}>
+                <td>{row.gene_id}</td>
+                <td>{row.group_id}</td>
+                <td>{row.timepoint_id}</td>
+                <td>{row.cell_type}</td>
+                <td>{row.value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
