15589831eb929be70a415b7b16d4ff150dfa3e93
Adam Dinan <adam1989ie@gmail.com>
2023-05-08 17:17:45 +0100

    Fix app import for tests

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,11 +1,7 @@
 import os
 import sys
 import pytest
-
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'routes')))
-
-from routes import ExpressionValue
+from app import routes
 
 @pytest.fixture
 def client():
--- server/tests/test_routes.py
+++ server/tests/test_routes.py
@@ -1,12 +1,7 @@
 import os
 import sys
 import pytest
-
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
-
-from app import app, db
-
+from app import app
 
 def test_gene_expression_route(client):
     response = client.get('/api/gene_expression?gene=TLR5')
