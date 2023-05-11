03e2bab2257b4069f21c7db08bbb6c1088e2e248
Adam Dinan <adam1989ie@gmail.com>
2023-05-11 12:51:26 +0100

    Activate venv in wsgi script

--- server/app/wsgi.py
+++ server/app/wsgi.py
@@ -2,7 +2,11 @@ import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
 
-sys.path.insert(0, '/var/www/cf-express-backend/')
+activate_this = '/var/www/html/cf-express-backend/venv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+sys.path.insert(0, '/var/www/html/cf-express-backend/')
 
 from app import create_app
 application = create_app()
