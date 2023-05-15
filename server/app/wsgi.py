d6315192fd405651233ebb254d65b369c73f6326
Adam Dinan <adam1989ie@gmail.com>
2023-05-15 11:20:57 +0100

    Update path for WSGI server

--- server/app/wsgi.py
+++ server/app/wsgi.py
@@ -3,10 +3,12 @@ import logging
 logging.basicConfig(stream=sys.stderr)
 
 activate_this = '/var/www/html/cf-express-backend/venv/bin/activate_this.py'
+
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
-sys.path.insert(0, '/var/www/html/cf-express-backend/')
+sys.path.insert(0, '/var/www/cf-express-backend/app')
 
 from app import create_app
 application = create_app()
+app, db = application
